28c192173b58c59f307c3f533c2301f8571054b9
Vu Cu <cxv@g.ucla.edu>
2017-11-13 11:17:57 +0700

    Display table

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'table-component',
   templateUrl: './table.component.html'
 })
-export class TableComponent {
+export class TableComponent implements OnInit {
+  table:number[][]
+  cols = 5;
+  rows = 20;
+
+  ngOnInit() {
+    let i,j:number;
+    for (i=0;i<this.cols;i++) {
+      let a:number[];
+      for (j=0;j<this.rows;j++) {
+        a.push(Math.random()*100);
+      }
+      this.table.push(a);
+    }
+  }
+
 }
