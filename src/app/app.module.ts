89c8710cbe8f969d167851f78bbdff88064b24a0
Vu Cu <cxv@g.ucla.edu>
2017-11-13 10:38:49 +0700

    Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {HomeComponent} from "./pages/home/home.component";
+import {TableComponent} from "./pages/table/table.component";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pages/table/table.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'table-component',
+  templateUrl: './table.component.html'
+})
+export class TableComponent {
+}
